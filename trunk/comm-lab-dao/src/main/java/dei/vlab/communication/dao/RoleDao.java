236d198b193b316409b943d77d26ce580d7a2a73
shibunair77 <shibunair77@5027c776-57f2-1da6-8343-c9cb2a8860fd>
2011-12-23 23:46:50 +0000

    added dao code

---
+++ trunk/comm-lab-dao/src/main/java/dei/vlab/communication/dao/RoleDao.java
@@ -0,0 +1,23 @@
+package dei.vlab.communication.dao;
+
+import dei.vlab.communication.model.Role;
+
+/**
+ * Role Data Access Object (DAO) interface.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+public interface RoleDao extends GenericDao<Role, Long> {
+    /**
+     * Gets role information based on rolename
+     * @param rolename the rolename
+     * @return populated role object
+     */
+    Role getRoleByName(String rolename);
+
+    /**
+     * Removes a role from the database by name
+     * @param rolename the role's rolename
+     */
+    void removeRole(String rolename);
+}
---
+++ trunk/comm-lab-dao/src/main/java/dei/vlab/communication/model/User.java
@@ -0,0 +1,175 @@
+package dei.vlab.communication.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+/**
+ * This class represents the basic "user" object in AppFuse that allows for authentication
+ * and user management.  It implements Acegi Security's UserDetails interface.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ *         Updated by Dan Kibler (dan@getrolling.com)
+ *         Extended to implement Acegi UserDetails interface
+ *         by David Carter david@carter.net
+ */
+@Entity
+@Table(name = "user")
+
+@XmlRootElement
+public class User extends BaseObject implements Serializable {
+    private static final long serialVersionUID = 3832626162173359411L;
+
+    private Long id;
+    private String username;                    // required
+    private String password;                    // required
+    private String status;						// required
+    private Set<Role> roles = new HashSet<Role>();
+ 
+    /**
+     * Default constructor - creates a new instance with no values set.
+     */
+    public User() {
+    }
+
+    /**
+     * Create a new instance and set the username.
+     *
+     * @param username login name for user.
+     */
+    public User(final String username) {
+        this.username = username;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    @Column(nullable = false, length = 50, unique = true)
+    public String getUsername() {
+        return username;
+    }
+
+    @Column(nullable = false)
+    @XmlTransient
+    public String getPassword() {
+        return password;
+    }
+
+ 
+  
+   
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    /**
+     * Adds a role for the user
+     *
+     * @param role the fully instantiated role
+     */
+    public void addRole(Role role) {
+        getRoles().add(role);
+    }
+   
+
+  
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+   
+  	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+  	 
+    @Column(nullable = false, length = 50, unique = false)
+    public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	/**
+     * {@inheritDoc}
+     */
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+
+        final User user = (User) o;
+
+        return !(username != null ? !username.equals(user.getUsername()) : user.getUsername() != null);
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int hashCode() {
+        return (username != null ? username.hashCode() : 0);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String toString() {
+        ToStringBuilder sb = new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE)
+                .append("username", this.username);
+        if (roles != null) {
+            sb.append("Granted Authorities: ");
+
+            int i = 0;
+            for (Role role : roles) {
+                if (i > 0) {
+                    sb.append(", ");
+                }
+                sb.append(role.toString());
+                i++;
+            }
+        } else {
+            sb.append("No Granted Authorities");
+        }
+        return sb.toString();
+    }
+}
